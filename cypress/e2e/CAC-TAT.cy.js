d8f304cc70babafa66e704340b3428cc77891e1e
Carolina Campos <carolinacampos.dev@gmail.com>
2025-05-03 21:34:31 -0300

    Adiciona novos comandos aos testes
    As funcionalidades cy.clock() e cy.tick(), para verificar não só que a mensagem aparece, mas também que desaparece após 3 segundos.
    
    E também o comando Cypress._.times() para rodar um teste várias vezes

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,41 +1,49 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
-  
+
   beforeEach(() => {
-   cy.visit('src/index.html')
+    cy.visit('src/index.html')
   })
 
   it('Verifica o título da aplicação', () => {
     cy.title()
-    .should('eq', 'Central de Atendimento ao Cliente TAT' )
+      .should('eq', 'Central de Atendimento ao Cliente TAT')
+  })
+  Cypress._.times(2, () => {
+    it('preenche os campos obrigatórios e envia o formulário', () => {
+      cy.clock()
+      cy.get('#firstName').type('Carolina')
+      cy.get('#lastName').type('Campos')
+      cy.get('#email').type('carolina@teste.com')
+      cy.get('#open-text-area').type('Escrevendo um texto longo para testar o delay', { delay: 0 })
+      cy.contains('button', 'Enviar').click()
+      cy.get('.success').should('be.visible')
+      cy.tick(3000)
+      cy.get('.success').should('not.be.visible')
+    })
   })
 
-  it('preenche os campos obrigatórios e envia o formulário', () => {
-    cy.get('#firstName').type('Carolina')
-    cy.get('#lastName').type('Campos')
-    cy.get('#email').type('carolina@teste.com')
-    cy.get('#open-text-area').type('Escrevendo um texto longo para testar o delay', { delay: 0})
-    cy.contains('button', 'Enviar').click()
-
-    cy.get('.success').should('be.visible')
-  })
-
-  it('exibe mensagem de errro ao submeter o formulário em um email com formatação inválida', () => {
+  it.only('exibe mensagem de errro ao submeter o formulário em um email com formatação inválida', () => {
+    cy.clock()
     cy.get('#firstName').type('Carolina')
     cy.get('#lastName').type('Campos')
     cy.get('#email').type('carolinateste.com')
     cy.get('#open-text-area').type('Testando')
     cy.contains('button', 'Enviar').click()
-    
+
     cy.get('.error').should('be.visible')
+    cy.tick(3000)
+    cy.get('.error').should('not.be.visible')
   })
 
-  it('verifica se o campo telefone fica vazio digitando valor não-numérico' , () => {
+
+  it('verifica se o campo telefone fica vazio digitando valor não-numérico', () => {
     cy.get('#phone')
       .type('abc')
       .should('have.value', '')
   })
 
-  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+  it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.clock()
     cy.get('#firstName').type('Carolina')
     cy.get('#lastName').type('Campos')
     cy.get('#email').type('carolina@teste.com')
@@ -44,6 +52,8 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.contains('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
+    cy.tick(3000)
+    cy.get('.error').should('not.be.visible')
   })
 
   it('preenche e limpa os campos nome, sonbrenome, email e telefone', () => {
@@ -70,19 +80,22 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   })
 
   it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
-      cy.contains('button', 'Enviar').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
   })
 
-  it('envia o formuário com sucesso usando um comando customizado1', () => {
+  it.only('envia o formuário com sucesso usando um comando customizado1', () => {
+    cy.clock()
     cy.fillMandatoryFieldsAndSubmit()
 
     cy.get('.success').should('be.visible')
+    cy.tick(3000)
+    cy.get('.success').should('not.be.visible')
   })
 
   it('envia o formuário com sucesso usando um comando customizado2', () => {
-    const data= {
+    const data = {
       firstName: 'Carol',
       lastName: 'Alves',
       email: 'carolina@teste.com',
@@ -124,7 +137,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
         cy.wrap(supportType)
           .check()
           .should('be.checked')
-    })
+      })
   })
 
   it('marca ambos checkboxes, depois desmarca o último', () => {
@@ -139,33 +152,33 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   it('seleciona um arquivo da pasta fixtures', () => {
     cy.get('#file-upload')
       .selectFile('cypress/fixtures/example.json')
-      .should ( input => {
+      .should(input => {
         expect(input[0].files[0].name).to.equal('example.json')
       })
   })
 
   it('seleciona um arquivo simulando um drag-and-drop', () => {
     cy.get('#file-upload')
-      .selectFile('cypress/fixtures/example.json', { action: 'drag-drop'})
-      .should ( input => {
+      .selectFile('cypress/fixtures/example.json', { action: 'drag-drop' })
+      .should(input => {
         expect(input[0].files[0].name).to.equal('example.json')
       })
   })
 
-    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+  it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
     cy.fixture('example.json').as('sampleFile')
     cy.get('#file-upload')
       .selectFile('@sampleFile')
-      .should ( input => {
+      .should(input => {
         expect(input[0].files[0].name).to.equal('example.json')
       })
   })
-  
+
   it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
     cy.contains('a', 'Política de Privacidade')
       .should('have.attr', 'href', 'privacy.html')
       .and('have.attr', 'target', '_blank')
-  }) 
+  })
 
   it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
     cy.contains('a', 'Política de Privacidade')
